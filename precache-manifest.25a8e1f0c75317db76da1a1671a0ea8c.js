e3c356e935c3530ffe0da83a12afdb8db5a5f166
BarBlgn <33877322+BarBlgn@users.noreply.github.com>
2020-06-11 20:34:04 +0300

    Updates

---
+++ precache-manifest.25a8e1f0c75317db76da1a1671a0ea8c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd38aef274964659e9fbfd8f875f0d2d",
+    "url": "/youtube_movies/index.html"
+  },
+  {
+    "revision": "1684d359b17254b4e596",
+    "url": "/youtube_movies/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "58110760768a5fcef9b3",
+    "url": "/youtube_movies/static/css/main.a0a65519.chunk.css"
+  },
+  {
+    "revision": "1684d359b17254b4e596",
+    "url": "/youtube_movies/static/js/2.9ccfc6bf.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/youtube_movies/static/js/2.9ccfc6bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58110760768a5fcef9b3",
+    "url": "/youtube_movies/static/js/main.d72aeaad.chunk.js"
+  },
+  {
+    "revision": "0d10f756db77eb700686",
+    "url": "/youtube_movies/static/js/runtime-main.9eb4ed1a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/youtube_movies/static/media/logo.5d5d9eef.svg"
+  }
+]);
